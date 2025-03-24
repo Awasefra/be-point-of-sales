bec933e04ddd3f93e59bd2aa6994e164b0f62d6c
awasefra <awandasetya@gmail.com>
2025-03-24 13:42:21 +0700

    feat(base): add cors and body parser

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
-//import 
-import dotenv from 'dotenv';
+//import
+import dotenv from "dotenv";
 import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
 
 dotenv.config();
 
-//init app 
-const app = express()
-
-//route
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+//init app
+const app = express();
 
 //port
 const PORT = process.env.PORT || 5000;
 
+//use cors
+app.use(cors());
+
+//use body parser
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+//route
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 app.listen(PORT, () => {
-    console.log("Server running on port 3000");
-  });
+  console.log("Server running on port 3000");
+});
